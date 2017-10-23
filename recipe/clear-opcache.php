ea31b99960e653ae7979ab8498d6b7dad48985c0
Martin Kirilov <wucdbm@gmail.com>
2017-10-24 02:13:29 +0300

    add doc

--- recipe/clear-opcache.php
+++ recipe/clear-opcache.php
@@ -2,6 +2,9 @@
 
 namespace Deployer;
 
+// Note: For cgi-fcgi to work, the below package needs to be installed on Ubuntu-based systems
+// apt-get install libfcgi0ldbl
+
 set('clear_opcache_php_sock', '/var/run/php/php7.1-{{user}}.sock');
 task('deploy:opcache:clear', function () {
     $command = 'SCRIPT_NAME=/opcache.php SCRIPT_FILENAME={{deploy_path}}/current/opcache.php REQUEST_METHOD=GET cgi-fcgi -bind -connect {{clear_opcache_php_sock}}';
